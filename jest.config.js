91d0fcfe3e1eca61ef93c90b3e0a5b2c523736f9
Kei Kamikawa <code-hex@users.noreply.github.com>
2020-06-24 23:31:28 +0900

    added tests and fixed code

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig-test.json"
+    }
+  },
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
   testEnvironment: 'node',
